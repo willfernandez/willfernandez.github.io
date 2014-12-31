07f5b24165b5092de79066b382a05de4cc2d7c33
Willy Fernández <willyfernandez12@gmail.com>
2014-12-31 16:33:12 -0500

    background del section presentacion

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ function windowContainerSizing(){
 	var windowHeight=$(window).height();
 	var headerHeight=$('header').height();
 	var heroHeight=windowHeight-headerHeight;
-	//var openerCopyHeight=$('.opener_copy').height();
-	var workDescription=$('.work li p').height();
+	var boxpresentacionHeight=$('.box-presentacion').height();
+	//var workDescription=$('.work li p').height();
 	$('.presentacion').css({top:headerHeight,height:heroHeight});
-	//$('.opener_copy').css({marginTop:(openerCopyHeight/2)*-1});
+	$('.box-presentacion').css({marginTop:(boxpresentacionHeight/2)*-1});
 	$('.about').css({marginTop:heroHeight+headerHeight});
 };
 
