ce0cffdb50a2b89411dc972f5921ab43663d1ee7
Willy Fernández <willyfernandez12@gmail.com>
2015-01-19 20:45:52 -0500

    agregando animaciones
    animate, scrollto

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
+$( document ).ready(function() {
+    jQuery(function($) { 
+		     $('.about p').waypoint(function() {
+		        $(this).toggleClass( 'fadeInDown animated' );
+		    },
+		    {
+		        offset: '70%',
+		        triggerOnce: true
+		    });
+
+		     $('.trabajos .pProyecto').waypoint(function() {
+		        $(this).toggleClass( 'rotateIn  animated' );
+		    },
+		    {
+		        offset: '70%',
+		        triggerOnce: true
+		    });
+
+		     $('h3').waypoint(function() {
+		        $(this).toggleClass( 'fadeInLeft  animated' );
+		    },
+		    {
+		        offset: '70%',
+		        triggerOnce: true
+		    });
+		     $('h1').waypoint(function() {
+		        $(this).toggleClass( 'lightSpeedIn  animated' );
+		    },
+		    {
+		        offset: '70%',
+		        triggerOnce: true
+		    });
+    
+	});
+	
+});
+
 function windowContainerSizing(){
 	var windowHeight=$(window).height();
 	var headerHeight=$('header').height();
@@ -13,4 +50,17 @@ windowContainerSizing();
 
 $(window).on('resize',function(){
 	windowContainerSizing();
+});
+
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
 });
